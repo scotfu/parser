4627128ebb549778ef22211aa2959ad9bcd50b6b
scot <scotfu@gmail.com>
2014-04-04 12:57:51 -0400

    update

--- python/p_flex.py
+++ python/p_flex.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
 
+def groupnize(data):
+    global_conf = re.search(r'(global.*?\{.*?\};?)', data, re.DOTALL)
+    host_confs = re.search(r'(host.*\{.*\};?\n*)', data, re.DOTALL)
+    return global_conf,host_confs
 
-def tokenize(str):
-    pass
+def process(data):
+    t = groupnize(data)
+    global_conf = t[0].group()
+    host_confs = t[1].group()
+    print global_conf
+    print host_confs
 
-
-
-if __name__ == '__main__':
+def readData():
     file_name = 'test.cfg'
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
     if not os.path.isfile(file_name):
         print 'ERR:F:\n'
         exit()
-
-    data = open(file_name).read()
-    tokenize(data)
+    f = open(file_name) 
+    data = f.read()
+    f.close()
+    return data
+    
+    
+if __name__ == '__main__':
+    data = readData()
+    #print data
+    process(data)
