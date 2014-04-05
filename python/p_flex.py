67b5ea7fe438b0aada0317e5ab7499087f721412
fu <scotfu@gmail.com>
2014-04-05 15:33:41 -0400

    token analyzer done

--- python/p_flex.py
+++ python/p_flex.py
@@ -2,11 +2,93 @@
 import os
 import sys
 
+#Types for nodes
+COMMENT = 1
+QUOTED_STRING = 2
+STRING = 3
+LEFT = 4
+RIGHT = 5
+SEMI = 6
+NEW_LINE = 7
+EQUAL = 8
+
+
+
+class Node:
+    def __init__(self,type,value=None):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return str(self.type) + ':' +str(self.value)
+    
+def quoted_string(str): # pass anything after a "
+    n = 0
+    while True:
+        if str[n] != '"':
+            n +=1
+        else:
+            if str[n-1] == '\\':
+                n += 1
+            else:
+                val = str[:n]
+                break
+    return str[n+1:],val        
+
+def comment(str):
+    while str[0] != '\n':
+        str = str[1:]
+    return str    
 
 def tokenize(str):
-    pass
-
-
+    tokens = []
+    # test for null
+    if '\0' in str:
+        raise 'Found null'
+    while len(str):
+        val = ''
+        if str[0] == '"':#take care of quoted strings
+            str, val = quoted_string(str)
+            tokens.append(Node(QUOTED_STRING,val))
+        elif str[0] == '#':#take care of comments
+            str = comment(str)
+            tokens.append(Node(COMMENT))
+        elif str[0] == '=':#equal sign
+            str = str[1:]
+            val = '='
+            tokens.append(Node(EQUAL,val))
+        elif str[0] == '{':#right
+            str = str[1:]
+            val = '{'
+            tokens.append(Node(LEFT,val))
+        elif str[0] == '}':#left
+            str = str[1:]
+            val = '}'
+            tokens.append(Node(RIGHT,val))
+        elif str[0] == ';':#semicolon
+            str = str[1:]
+            val = ';'
+            tokens.append(Node(SEMI,val))
+        elif str[0] == '\n':#new line
+            str = str[1:]
+            val = '\n'
+            tokens.append(Node(NEW_LINE,val))
+        elif str[0] == ' ':#space bug?
+            str = str[1:]
+        elif str[0].isalpha() or str[0].isdigit() or (str[0] in ['-','_','.','/']):#any other string,etc.key,value,host,gloal
+            val = str[0]
+            str = str[1:]
+            while len(str):
+                if str[0] not in [' ','{','}',';','=','\n']:
+                    val += str[0]
+                    str = str[1:]
+                else:
+                    tokens.append(Node(STRING,val))
+                    break
+        else:
+            print 'Warning, invalide characters',str[0]
+            str = str[1:]
+    return tokens
 
 if __name__ == '__main__':
     file_name = 'test.cfg'
@@ -17,4 +99,5 @@ if __name__ == '__main__':
         exit()
 
     data = open(file_name).read()
-    tokenize(data)
+    import pprint
+    pprint.pprint(tokenize(data))
