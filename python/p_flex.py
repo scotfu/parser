d5c58bbe2b506b30dbcfaf6e384e5f1190918b65
fu <scotfu@gmail.com>
2014-04-06 16:42:46 -0400

    class based tokenizer started

--- python/p_flex.py
+++ python/p_flex.py
@@ -28,77 +28,81 @@ class Node:
 
     def __repr__(self):
         return str(self.type) + ':' +str(self.value)
-    
-def quoted_string(str): # pass anything start from " "
-    n = 1
-    while True:
-        if str[n] == '\n':
-            raise FlexException
-        elif str[n] != '"':
-            n += 1
-        else:
-            if str[n-1] == '\\':
+
+class Tokenizer:
+    def __init__(self,data_str):
+        self.data_str = data_str
+        self.lineno = 1
+        self.tokens = []    
+
+    def quoted_string(self): # pass anything start from " "
+        n = 1
+        while True:
+            if self.data_str[n] == '\n':
+                raise FlexException
+            elif self.data_str[n] != '"':
                 n += 1
             else:
-                val = str[:n]
-                break
-    return str[n+1:],val        
-
-def comment(str):
-    while str[0] != '\n':
-        str = str[1:]
-    return str    
-
-def tokenize(str):
-    tokens = []
-
-    # test for null
-    if '\0' in str:
-        raise NullException
-    while len(str):
-        val = ''
-        if str[0] == '"':#take care of quoted strings
-            str, val = quoted_string(str)
-            tokens.append(Node(QUOTED_STRING,val))
-        elif str[0] == '#':#take care of comments
-            str = comment(str)
-            tokens.append(Node(COMMENT))
-        elif str[0] == '=':#equal sign
-            str = str[1:]
-            val = '='
-            tokens.append(Node(EQUAL,val))
-        elif str[0] == '{':#right
-            str = str[1:]
-            val = '{'
-            tokens.append(Node(LEFT,val))
-        elif str[0] == '}':#left
-            str = str[1:]
-            val = '}'
-            tokens.append(Node(RIGHT,val))
-        elif str[0] == ';':#semicolon
-            str = str[1:]
-            val = ';'
-            tokens.append(Node(SEMI,val))
-        elif str[0] == '\n':#new line
-            str = str[1:]
-            val = '\n'
-            tokens.append(Node(NEW_LINE,val))
-        elif str[0] == ' ':#space bug?
-            str = str[1:]
-        elif str[0].isalpha() or str[0].isdigit() or (str[0] in ['-','_','.','/']):#any other string,etc.key,value,host,gloal
-            val = str[0]
-            str = str[1:]
-            while len(str):
-                if str[0] not in [' ','{','}',';','=','\n']:
-                    val += str[0]
-                    str = str[1:]
+                if self.data_str[n-1] == '\\':
+                    n += 1
                 else:
-                    tokens.append(Node(STRING,val))
+                    val = self.data_str[:n]
                     break
-        else:
-            print 'Warning, invalide characters',str[0]
-            str = str[1:]
-    return tokens
+        self.data_str = self.data_str[n+1:]
+        return val        
+
+    def comment(self):
+        while self.data_str[0] != '\n':
+            self.data_str = self.data_str[1:]
+            
+    def tokenize(self):
+        if '\0' in self.data_str:
+            raise NullException
+        while len(self.data_str):
+            print len(self.data_str)
+            val = ''
+            if self.data_str[0] == '"':#take care of quoted self.data_strings
+                val = self.quoted_string()
+                self.tokens.append(Node(QUOTED_STRING,val))
+            elif self.data_str[0] == '#':#take care of comments, \n extracted from comments
+                self.comment()
+                self.tokens.append(Node(COMMENT))
+            elif self.data_str[0] == '=':#equal sign
+                self.data_str = self.data_str[1:]
+                val = '='
+                self.tokens.append(Node(EQUAL,val))
+            elif self.data_str[0] == '{':#right
+                self.data_str = self.data_str[1:]
+                val = '{'
+                self.tokens.append(Node(LEFT,val))
+            elif self.data_str[0] == '}':#left
+                self.data_str = self.data_str[1:]
+                val = '}'
+                self.tokens.append(Node(RIGHT,val))
+            elif self.data_str[0] == ';':#semicolon
+                self.data_str = self.data_str[1:]
+                val = ';'
+                self.tokens.append(Node(SEMI,val))
+            elif self.data_str[0] == '\n':#new line
+                self.data_str = self.data_str[1:]
+                val = '\n'
+                self.lineno += 1
+                self.tokens.append(Node(NEW_LINE,val))
+            elif self.data_str[0] == ' ':#space bug?
+                self.data_str = self.data_str[1:]
+            elif self.data_str[0].isalpha() or self.data_str[0].isdigit() or (self.data_str[0] in ['-','_','.','/']):#any other self.data_string,etc.key,value,host,gloal
+                val = self.data_str[0]
+                self.data_str = self.data_str[1:]
+                while len(self.data_str):
+                    if self.data_str[0] not in [' ','{','}',';','=','\n']:
+                        val += self.data_str[0]
+                        self.data_str = self.data_str[1:]
+                    else:
+                        self.tokens.append(Node(STRING,val))
+                        break
+            else:
+                print 'Warning, invalide characters',self.data_str[0],self.lineno
+                self.data_str = self.data_str[1:]
 
 if __name__ == '__main__':
     file_name = 'test.cfg'
@@ -111,7 +115,8 @@ if __name__ == '__main__':
     data = open(file_name).read()
     import pprint
     try:
-        pprint.pprint(tokenize(data))
-    except FlexException,lineno:
-        print 'E:L:%d'%lineno
+        tokenizer = Tokenizer(data)
+        tokenizer.tokenize()
+    except FlexException:
+        print 'E:L:%d'%tokenizer.lineno
         
