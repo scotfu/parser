3eaf54d13e519b38c47bcc0bff62a1f9415dcae5
fu <scotfu@gmail.com>
2014-04-28 10:09:07 -0400

    todo recovery

--- scheme/imp2.scm
+++ scheme/imp2.scm
@@ -34,7 +34,7 @@
 (define tokens `())
 (define global_keys (make-list 0))
 (define global_node_list (make-list 0))
-
+(define global_lineno 1)
 (define-class <node> ()
   (type #:init-value 0 #:getter get-type #:setter set-type! #:init-keyword #:type)
   (value #:init-value 0 #:getter get-value #:setter set-value! #:init-keyword #:value)
@@ -55,15 +55,17 @@
     (set! file_name (cadr(command-line)))
 )
 )
-
+;
 ;test file if exists
 (define (test_file file)
 (if (file-exists? file_name)
 (begin 
 (values))
 ;do something here
-
+(begin
 (display "ERR:F\n");todo exit here?
+(exit)
+)
 )
 )
 
@@ -142,17 +144,30 @@ contents
       (output `())
       ) 
 (while (> 1 0)
-(if (not(char=? #\" (list-ref str n)))
+(cond
+ ((char=? #\newline (list-ref str (- n 1)))
+  (begin
+    (display "ERR:L:")
+    (display global_lineno)
+    (newline)
+    (exit)
+    ))
+
+ ((char>? (list-ref str (- n 1)) #\delete)
+  (begin
+    (display "ERR:L:")
+    (display global_lineno)
+    (newline)
+    (exit)
+    ))
+ 
+ ((not(char=? #\" (list-ref str n)))
     (begin
     (set! n (+ n 1))
-;    (newline)
-;    (display "I am showing n\n")
-;    (display n)
-;    (newline)
 
-    )
-
-    (begin
+    ))
+ (else
+  (begin
     (if (char=? #\\ (list-ref str (- n 1)))
          (set! n (+ n 1))
          (begin 
@@ -163,7 +178,7 @@ contents
                 )
          );if
      
-     );begin
+     ));begin
     );if
 )
 (set! output (list str var))
@@ -238,7 +253,14 @@ output
 (let (
        (str_list (string->list str))
        (tokens (make-list 0))
-       (lineno 1)
+;       (lineno 1)
+)
+(if (char_in #\null str_list)
+(begin
+    (display "ERR:L:");bug here no lineno
+    (display global_lineno)
+    (newline)
+    (exit))
 )
 
 (while (> (length str_list) 0)
@@ -254,13 +276,15 @@ output
        ((char=? mchar #\") (begin ;(display #\")
                                   (set! tmp (quoted_string str_list))
                                   (set! str_list (car tmp))
-                                  (set! var (cdr tmp))
+                                  (set! var (car (cdr tmp)))
                                   (set! tokens (append tokens (make-list 1 
                                                                          (make <node> #:value var
-                                                                               #:lineno lineno
+                                                                               #:lineno global_lineno
                                                                                #:type QUOTED_STRING))))
                                   ;(display var)
-                                  ))
+                                  )
+;        (display "ERR:L\n")
+        )
        ((char=? mchar #\#) (begin 
                              ;(display "# ")
                              ;remove valid comment
@@ -277,7 +301,7 @@ output
 
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value #\{
-                                                                             #:lineno lineno
+                                                                             #:lineno global_lineno
                                                                              #:type LEFT))))
                                   ))
        ((char=? mchar #\}) (begin 
@@ -285,7 +309,7 @@ output
                                   (set! str_list (cdr str_list))
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value #\}
-                                                                             #:lineno lineno
+                                                                             #:lineno global_lineno
                                                                              #:type RIGHT))))
 ;                                  (set! tokens (append tokens (list mchar)))
                                   ))
@@ -295,7 +319,7 @@ output
 
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value #\;
-                                                                             #:lineno lineno
+                                                                             #:lineno global_lineno
                                                                              #:type SEMI))))
 ;                                  (set! tokens (append tokens (list mchar)))
                                   ))
@@ -311,8 +335,8 @@ output
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value "newline"
                                                                              #:type NEW_LINE
-                                                                             #:lineno lineno))))
-                                  (set! lineno (+ 1 lineno))
+                                                                             #:lineno global_lineno))))
+                                  (set! global_lineno (+ 1 global_lineno))
 
                                         ))
        ((char=? mchar #\=) (begin 
@@ -321,21 +345,29 @@ output
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value #\=
                                                                              #:type EQUAL
-                                                                             #:lineno lineno))))
+                                                                             #:lineno global_lineno))))
 
                                         ))
 
        ((char<=? mchar #\delete) (begin 
                                   (set! tmp (n_string str_list))
-                                  (set! var (cdr tmp))
+                                  (set! var (car (cdr tmp)))
                                   (set! str_list (car tmp))
                                   (set! tokens (append tokens (make-list 1 (make <node> 
                                                                              #:value var
                                                                              #:type STRING
-                                                                             #:lineno lineno))))
+                                                                             #:lineno global_lineno))))
                                   ;(display var)
 ;                                  (display (list? var))
                                         ))
+       (else
+        (begin
+          (display "ERR:P:")
+          (display (get-lineno (car tokens)))
+          (newline)
+          (exit)
+          )
+        )
         )
 );let
 );while
@@ -350,6 +382,7 @@ tokens
 (while (and (not(null? tokens))  (eqv? (get-type (car tokens)) NEW_LINE))
        (set! tokens (cdr tokens))
 )
+
 tokens
 )
 )
@@ -360,10 +393,10 @@ tokens
 (cond (
        (and (not (null? tokens))
             (eqv? STRING (get-type (car tokens)))
-            (regexp-match? (string-match "^[a-zA-Z_][a-zA-Z_0-9]*$" (car (get-value (car tokens))))))
+            (regexp-match? (string-match "^[a-zA-Z_][a-zA-Z_0-9]*$" (get-value (car tokens)))))
 
        (begin 
-         (slot-set! currnode `c1 (make <node> #:type KEY #:value (car (get-value (car tokens)))))
+         (slot-set! currnode `c1 (make <node> #:type KEY #:value  (get-value (car tokens))))
          (set! tokens (cdr tokens))
          (letrec (
                   (node_type 0)
@@ -384,7 +417,7 @@ tokens
             (cond
              
              (
-              (regexp-match? (string-match "^[+-]?[0-9]+$" (car (get-value (car tokens)))))
+              (regexp-match? (string-match "^[+-]?[0-9]+$" (get-value (car tokens))))
               (begin 
                 (set! node_type INT)
                 (set! prefix "I::")
@@ -392,7 +425,7 @@ tokens
               );int
              ((and (not (null? tokens))
                    (eqv? STRING (get-type (car tokens)))
-                   (regexp-match? (string-match "^[+-]?[0-9]+\\.[0-9]*$" (car (get-value (car tokens)))))
+                   (regexp-match? (string-match "^[+-]?[0-9]+\\.[0-9]*$" (get-value (car tokens))))
                    )
               (begin 
                 
@@ -402,7 +435,7 @@ tokens
              
              ((and (not (null? tokens))
                    (eqv? STRING (get-type (car tokens)))
-                   (regexp-match? (string-match "^[/a-zA-Z][/\\._a-zA-Z0-9\\-]*$" (car (get-value (car tokens)))))
+                   (regexp-match? (string-match "^[/a-zA-Z][/\\._a-zA-Z0-9\\-]*$" (get-value (car tokens))))
                    )
               (begin 
                 
@@ -410,16 +443,33 @@ tokens
                 (set! prefix "S::")
                 )
               );string
+
+             (else
+              (begin
+                (display "ERR:P:")
+                (display (get-lineno (car tokens)))
+                (newline)
+                (exit)
+                )
+              )
              )
-            (set-c3! currnode (make <node> #:type node_type #:value (car (get-value (car tokens)))))
+            (set-c3! currnode (make <node> #:type node_type #:value (get-value (car tokens))))
             (set! tokens (cdr tokens))
             ))
          
          ((and (not (null? tokens)) (eqv? QUOTED_STRING (get-type (car tokens))))
 
-          (set-c3! currnode (make <node> #:type QUOTED_STRING #:value (format_quoted_string  (car (get-value (car tokens))))))
+          (set-c3! currnode (make <node> #:type QUOTED_STRING #:value (format_quoted_string (get-value (car tokens)))))
           (set! tokens (cdr tokens))
           (set! prefix "Q::")
+          )
+         (else
+          (begin
+            (display "ERR:P:")
+            (display (get-lineno (car tokens)))
+            (newline)
+            (exit)
+            )
           )
          )
         (if (and (char_in (get-value (get-c1 currnode)) global_keys)
@@ -428,7 +478,16 @@ tokens
             (set! prefix (string-append (string-take prefix 2) "O" (string-take-right prefix 1)))
             ))
         (set-value! (get-c1 currnode) (string-append "    " prefix (get-value (get-c1 currnode))))
-));END OF IF
+)
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
+
+);END OF IF
     (if (not (null? tokens))
         (cond
          ((eqv? NEW_LINE (get-type (car tokens)))
@@ -437,9 +496,25 @@ tokens
          ((eqv? RIGHT (get-type (car tokens)))
           (values)
           )
+         (else    (begin
+                    (display "ERR:P:")
+                    (display (get-lineno (car tokens)))
+                    (newline)
+                    (exit)
+                    ))
+
          ))
     )))
       ((eqv? RIGHT (get-type (car tokens)))(values))
+      (else
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+)
+
       )
 tokens
 )
@@ -475,24 +550,39 @@ tokens
 (lambda (tokens currnode)
 
 (set! tokens (clean_newline tokens))
-(if (and (not(null? tokens)) (equal? "host" (car (get-value (car tokens)))) )
+(if (and (not(null? tokens)) (equal? "host" 
+                                             (get-value (car tokens))))
     (begin
       (set! tokens (cdr tokens))
       (slot-set! currnode `c1 (make <node> #:value "HOST" #:type HOST_KEYWORD))
 
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 
 )
 
 (set! tokens (clean_newline tokens))
 (if (and (not(null? tokens)) 
-         (regexp-match? (string-match "^[a-zA-Z_0-9\\._\\-]*$" (car (get-value (car tokens))))))
+         (regexp-match? (string-match "^[a-zA-Z_0-9\\._\\-]*$" (get-value (car tokens)))))
     (begin
-      (slot-set! currnode `c2 (make <node> #:value (car (get-value (car tokens))) #:type HOST_NAME))
+      (slot-set! currnode `c2 (make <node> #:value (get-value (car tokens)) #:type HOST_NAME))
       (set! tokens (cdr tokens))
 
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 
 )
@@ -503,6 +593,13 @@ tokens
       (slot-set! currnode `c3 (car tokens))
       (set! tokens (cdr tokens))
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 )
 
@@ -516,6 +613,13 @@ tokens
       (slot-set! currnode `c5 (car tokens));todo c3
       (set! tokens (cdr tokens))
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 )
 
@@ -572,12 +676,18 @@ tokens
 (lambda (tokens currnode)
 
 (set! tokens (clean_newline tokens))
-(if (and (not(null? tokens)) (equal? "global" (car (get-value (car tokens)))) )
+(if (and (not(null? tokens)) (equal? "global" (get-value (car tokens)))) 
     (begin
       (set! tokens (cdr tokens))
       (slot-set! currnode `c1 (make <node> #:value "GLOBAL" #:type GLOBAL_KEYWORD))
 
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
     ;todo exception handler
 
 )
@@ -588,6 +698,13 @@ tokens
       (slot-set! currnode `c2 (car tokens))
       (set! tokens (cdr tokens))
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 )
 
@@ -601,6 +718,13 @@ tokens
       (slot-set! currnode `c4 (car tokens));todo c3
       (set! tokens (cdr tokens))
       )
+    (begin
+        (display "ERR:P:")
+        (display (get-lineno (car tokens)))
+        (newline)
+        (exit)
+        )
+
     ;todo exception handler
 )
 
@@ -611,6 +735,7 @@ tokens
       (set! tokens (cdr tokens))
 
       )
+
     ;optional, no exception here
 )
 tokens
